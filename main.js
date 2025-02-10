3b7512de62aa132c8f86dc190ec08e4960dbb158
kevkevv28 <141606450+kevkevv28@users.noreply.github.com>
2025-02-10 22:06:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 onload = () => {
   document.body.classList.remove("container");
-  document.getElementById("playButton").click();
+  document.addEventListener("click", function () {
+  let audio = document.getElementById("myaudio");
+  if (audio) {
+    audio.play().catch(error => console.log("Autoplay blocked:", error));
+  }
+}, { once: true }); // Ensures it runs only once
+
  
   setTimeout(function () {
     window.location.href = "lock.html"; // Change this to your desired page
